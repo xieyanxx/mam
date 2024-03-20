864640ad746cdc6d501a4259fe587e31585d8e73
xieyan <1107582086@qq.com>
2024-03-20 22:25:48 +0800

    fix: 修改合约

--- src/components/EthersContainer/address.ts
+++ src/components/EthersContainer/address.ts
@@ -5,21 +5,21 @@ import usdc from "@/assets/logo/usdc.png";
 
 // 测试
 export const network = "bsc";
-export const farmContractAddress = "0x4BF4F322F1ee0953A2EF12E2E34c99FF63631a95"; //farm合约地址
-export const poolContractAddress = "0x5D2D7f28F99C0CDf09D4175C159A505559A74c76"; //pool合约地址
+export const farmContractAddress = "0xF55f6528316c8FDaf47C531721f459733cC25055"; //farm合约地址
+export const poolContractAddress = "0x5fdEAA3073dea7b0bB20c3E18BF285dF1A97649A"; //pool合约地址
 export const routeContractAddress =
-  "0x62ae1cd50c392e78e9482a151240302A30610635";
+  "0x5975863B45c2cF941BF232029944a981975Ea439";
 export const factoryContractAddress =
-  "0x07820957A53A2Eea3d94d0fca9Cc693a23530ceD";
+  "0x74dB217b86341b8515b842775930f2b7D7Ac1b6A";
 export const readyContractAddress =
-  "0xe17A5A27438C13E072CB8E8585837955a082881F";
+  "0x4AF89146f057Bc09bF2829366353dcc75751F4c2";
 export const ChainToken = [
   {
     id: 1,
     src: block1,
     name: "MAMBA",
     name1: "Mamba",
-    address: "0x61b040e9630873138329E706f20ef534Bb77cB5E",
+    address: "0xd77cfa1C2A24AC939507632c833b3D623123eE7D",
     address1: "",
   },
   {
@@ -28,14 +28,15 @@ export const ChainToken = [
     name: "SEI",
     name1: "Sei",
     address: "0x0000000000000000000000000000000000000000",
-    address1: "0x149f9234B8327C7Bb279E9Fd100919fc0a3e2Abd",
+    address1: "0xFC0b7E9E72fA2c6e2181B1c57A6b862831AECF6B",
   },
+
   {
     id: 3,
     src: usdc,
     name: "USDC",
     name1: "Usdc",
-    address: "0x54317F70bBF9776C817BDA9455D8179C70c429aA",
+    address: "0xAb2f5A0E23FF19e3630C2eA0fE98382949995209",
     address1: "",
   },
   {
@@ -43,7 +44,15 @@ export const ChainToken = [
     src: usdt,
     name: "USDT",
     name1: "Usdt",
-    address: "0x3A989dc09dA5623A7c868061ff7ae0b7fA4A3EC3",
+    address: "0xaBBEd9873127e3d41d2C3dA17C5f6Ef4D60a788B",
+    address1: "",
+  },
+  {
+    id: 5,
+    src: sei1,
+    name: "WSEI",
+    name1: "Wsei",
+    address: "0xFC0b7E9E72fA2c6e2181B1c57A6b862831AECF6B",
     address1: "",
   },
 ];
