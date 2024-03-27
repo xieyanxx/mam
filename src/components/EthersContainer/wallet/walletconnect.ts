1fa5ffcac6db68248ab983169d7bf260dd4e16da
xieyan <1107582086@qq.com>
2024-03-27 14:08:07 +0800

    fix: 删除无用代码

--- src/components/EthersContainer/wallet/walletconnect.ts
+++ src/components/EthersContainer/wallet/walletconnect.ts
@@ -5,7 +5,7 @@ export function WalletConnect() {
   return new Promise(async (resolve, reject) => {
     const provider = await EthereumProvider.init({
       projectId: "07976a1d7be7cc4477daef9502263f3b", // required 需要自己去申请免费的，可用于多个项目
-      optionalChains: [1, 97],
+      optionalChains: [1, 97,713715],
       showQrModal: true,
       rpcMap: WALLETCONNECT_RPC_NODE,
       qrModalOptions: {
